c95b13a80474cba3fd886df4fe02236e96e01687
Dante Prins <54874549+DantePrins@users.noreply.github.com>
2024-02-10 14:13:17 -0800

    new version of my layout
    there are DRC errors but looking at the Si_core layer everything looks fine.

--- Layout/Designs/design_danteprins.py
+++ Layout/Designs/design_danteprins.py
@@ -1,13 +1,15 @@
 from pya import *
 
- 
+
 def design_danteprins(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
     
     # load functions
     from SiEPIC.scripts import connect_pins_with_waveguide, connect_cell
     ly = cell.layout()
     library = ly.technology().name
-    
+
+    cell_taper = ly.create_cell('ebeam_taper_350nm_2000nm_te1310', library)
+
     #####
     # designer circuit:
 
@@ -28,68 +30,108 @@ def design_danteprins(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type
          })
     if not cell_text:
         raise Exception ('Cannot load text label cell; please check the script carefully.')
-    cell.insert(CellInstArray(cell_text.cell_index(), Trans(Trans.R0, 25000,125000)))
-    
-    waveguide_type_mm = 'Strip TM 1550 nm, w=500 nm (core-clad)'                
+    cell.insert(CellInstArray(cell_text.cell_index(), Trans(Trans.R0, 25000,125000)))                
 
     # load the cells from the PDK
     # choose appropriate parameters
     cell_bragg = ly.create_cell('ebeam_pcell_bragg_grating', library, {
-        'number_of_periods':13,
-        'grating_period': 0.2587,
-        'corrugation_width': 0.2,
-        'wg_width': 0.5,
+        'number_of_periods':16,
+        'grating_period': 0.280,
+        'corrugation_width': 0.04,
+        'wg_width': 0.35,
         'sinusoidal': True})
     if not cell_bragg:
         raise Exception ('Cannot load Bragg grating cell; please check the script carefully.')
-
-    cell_taper = ly.create_cell('ebeam_pcell_taper', library, {
-        'wg_width1': 0.350,
-        'wg_width2': 0.5,
-            })
-    if not cell_taper:
-        raise Exception ('Cannot load taper cell; please check the script carefully.')
-
+    
+    waveguide_type_mm = 'Multimode Strip TE 1310 nm, w=2000 nm'
+    
     # instantiate y-branch (attached to input waveguide)
     inst_y1 = connect_cell(inst_wg1, 'opt2', cell_y, 'opt2')
 
-    # instantiate taper from 350 nm waveguide y-branch to 500 nm Bragg grating
-    inst_taper1 = connect_cell(inst_y1, 'opt1', cell_taper, 'pin1')
-    
     # instantiate Bragg grating (attached to y branch)
-    inst_bragg1 = connect_cell(inst_taper1, 'pin2', cell_bragg, 'opt1')
+    inst_bragg1 = connect_cell(inst_y1, 'opt1', cell_bragg, 'opt1')
 
-    # instantiate Bragg grating (attached to the first Bragg grating)
-    inst_bragg2 = connect_cell(inst_bragg1, 'opt2', cell_bragg, 'opt2')
-    
-    # move the Bragg grating to the right, and up
-    inst_bragg2.transform(Trans(250000,80000))
+    # instantiate a taper (attached to the first Bragg grating)
+    inst_taper1 = connect_cell(inst_bragg1, 'opt2', cell_taper, 'opt')
 
-    #####
-    # Waveguides for the two outputs:
+    # instantiate a taper (attached to the first taper, then move)
+    inst_taper2 = connect_cell(inst_taper1, 'opt2', cell_taper, 'opt2')
+    # move the taper to the right
+    inst_taper2.transform(Trans(230000,0))
+
+    # Waveguide between taper 1 and taper 2 (wide multimode waveguide)
+    connect_pins_with_waveguide(inst_taper1, 'opt2', inst_taper2, 'opt2', waveguide_type=waveguide_type_mm)
+
+    # instantiate a taper (attached to the first taper, then move)
+    inst_taper3 = connect_cell(inst_taper1, 'opt2', cell_taper, 'opt2')
+    # move the taper to the right and up
+    inst_taper3.transform(Trans(230000,20000))
+
+    # Waveguide between taper 2 and taper 3 (single mode waveguide)
+    connect_pins_with_waveguide(inst_taper2, 'opt', inst_taper3, 'opt', waveguide_type=waveguide_type)
+
+    # instantiate a taper (attached to the third taper, then move)
+    inst_taper4 = connect_cell(inst_taper3, 'opt2', cell_taper, 'opt2')
+    # move the taper to the right and up
+    inst_taper4.transform(Trans(-230000,0))
+
+    # Waveguide between taper 3 and taper 4 (wide multimode waveguide)
+    connect_pins_with_waveguide(inst_taper3, 'opt2', inst_taper4, 'opt2', waveguide_type=waveguide_type_mm)
+
+    # instantiate a taper (attached to the third taper, then move)
+    inst_taper5 = connect_cell(inst_taper3, 'opt2', cell_taper, 'opt2')
+    # move the taper to the right and up
+    inst_taper5.transform(Trans(-230000,20000))
+
+    # Waveguide between taper 4 and taper 5 (single mode waveguide)
+    connect_pins_with_waveguide(inst_taper4, 'opt', inst_taper5, 'opt', waveguide_type=waveguide_type)
+
+    # instantiate a taper (attached to the fifth taper, then move)
+    inst_taper6 = connect_cell(inst_taper5, 'opt2', cell_taper, 'opt2')
+    # move the taper to the right and up
+    inst_taper6.transform(Trans(200000,0))
+
+    # Waveguide between taper 5 and taper 6 (wide multimode waveguide)
+    connect_pins_with_waveguide(inst_taper5, 'opt2', inst_taper6, 'opt2', waveguide_type=waveguide_type_mm)
+
+    # instantiate a taper (attached to the 5th taper, then move)
+    inst_taper7 = connect_cell(inst_taper5, 'opt2', cell_taper, 'opt2')
+    # move the taper to the right and up
+    inst_taper7.transform(Trans(200000,20000))
+
+    # Waveguide between taper 6 and taper 7 (single mode waveguide)
+    connect_pins_with_waveguide(inst_taper6, 'opt', inst_taper7, 'opt', waveguide_type=waveguide_type)
+
+    # instantiate a taper (attached to the 7th taper, then move)
+    inst_taper8 = connect_cell(inst_taper7, 'opt2', cell_taper, 'opt2')
+    # move the taper to the right and up
+    inst_taper8.transform(Trans(-200000,0))
+
+    # Waveguide between taper 7 and taper 8 (wide multimode waveguide)
+    connect_pins_with_waveguide(inst_taper7, 'opt2', inst_taper8, 'opt2', waveguide_type=waveguide_type_mm)
+
+    # instantiate a taper (attached to the 7th taper, then move)
+    inst_taper9 = connect_cell(inst_taper7, 'opt2', cell_taper, 'opt2')
+    # move the taper to the right and up
+    inst_taper9.transform(Trans(-200000,20000))
+
+    # Waveguide between taper 8 and taper 9 (single mode waveguide)
+    connect_pins_with_waveguide(inst_taper8, 'opt', inst_taper9, 'opt', waveguide_type=waveguide_type)
+
+     # instantiate a taper (attached to the 9th taper, then move)
+    inst_taper10 = connect_cell(inst_taper9, 'opt2', cell_taper, 'opt2')
+    # move the taper to the right and up
+    inst_taper10.transform(Trans(200000,0))
+
+    # Waveguide between taper 9 and taper 10 (wide multimode waveguide)
+    connect_pins_with_waveguide(inst_taper9, 'opt2', inst_taper10, 'opt2', waveguide_type=waveguide_type_mm)
+
+    # instantiate Bragg grating (attached to the last taper)
+    inst_bragg2 = connect_cell(inst_taper10, 'opt', cell_bragg, 'opt2')
+
+    # End of Fabry-Perot cavity
+    # Waveguides for the two Fabry-Perot outputs:
     connect_pins_with_waveguide(inst_y1, 'opt3', inst_wg3, 'opt1', waveguide_type=waveguide_type)
-
-    # instantiate taper from 350 nm waveguide y-branch to 500 nm Bragg grating
-    inst_taper4 = connect_cell(inst_bragg2, 'opt1', cell_taper, 'pin2')
-
-    connect_pins_with_waveguide(inst_taper4, 'pin1', inst_wg2, 'opt1', waveguide_type=waveguide_type)
-    
-    '''
-    make a long waveguide, back and forth, 
-    target 0.2 nm FSR assuming ng = 4
-    > wavelength=1270e-9; ng=4; fsr=0.2e-9;
-    > L = wavelength**2/2/ng/fsr
-    > L * 1e6
-    > 1000 [microns]
-    using "turtle" routing
-    https://github.com/SiEPIC/SiEPIC-Tools/wiki/Scripted-Layout#adding-a-waveguide-between-components
-    '''
-    
-    try:
-        connect_pins_with_waveguide(inst_bragg1, 'opt2', inst_bragg2, 'opt2', waveguide_type=waveguide_type_mm, 
-            turtle_A = [335,90,40,90,355,-90,120,-90,50,-90,100,90,50,90,100,-90,50,-90,100,90,50,90,100,-90,50,-90] )
-    except:    
-         connect_pins_with_waveguide(inst_bragg1, 'opt2', inst_bragg2, 'opt2', waveguide_type=waveguide_type_mm, 
-            turtle_A = [320,90,40,90,360,-90,40,-90] )
+    connect_pins_with_waveguide(inst_bragg2, 'opt1', inst_wg2, 'opt1', waveguide_type=waveguide_type, turtle_A = [10, 90, 20, -90, 20, -90], error_min_bend_radius=False)
 
     return inst_wg1, inst_wg2, inst_wg3
